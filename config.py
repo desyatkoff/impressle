b65553bde09d5db0dd53e25d8e787f73401cca20
Desyatkov Sergey <sergeidesyatkov@gmail.com>
2025-01-26 13:15:00 +0500

    UPDATE: Updated config

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
 import os
 
+import dotenv
+
+
+dotenv.load_dotenv(".env")
+
 
 PROJECT_ROOT_PATH = os.path.dirname(__file__)
+
+FLASK_SECRET = os.getenv("FLASK_SECRET")
+SQLALCHEMY_DATABASE_URI = f"sqlite:///{PROJECT_ROOT_PATH}/database.db"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 HOST = "0.0.0.0"
 PORT = 5000
 DEBUG_MODE = False
