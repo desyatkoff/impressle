bd02f061578f5fd1396e3ad248d820cf6b54e6b1
Desyatkov Sergey <sergeidesyatkov@gmail.com>
2025-02-09 21:23:41 +0500

    UPDATE: Minor changes

--- website/auth.py
+++ website/auth.py
@@ -2,7 +2,7 @@
 #
 # Copyright (C) 2025 Desyatkov Sergey
 #
-# This file is part of impressle.
+# This file is part of Impressle.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
