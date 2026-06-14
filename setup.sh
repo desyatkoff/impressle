ef50e9facc8940290aac40458712bba2803631d0
Sergey Desyatkov <sergeydesyatkov@proton.me>
2026-06-14 19:01:24 +0500

    UPDATE: Switch from pipenv to uv + minor changes

--- setup.sh
+++ setup.sh
@@ -1,16 +1,6 @@
 #!usr/bin/env bash
 
-
 mv .env.example .env
-
 echo "$(cat .env)FLASK_SECRET" > .env
-
-pipx install pipenv
-pipx ensurepath
-
-pipenv lock
-pipenv sync
-
-pybabel compile -f -d translations/
-
-pipenv shell
+uv sync
+uv run pybabel compile -f -d translations/
